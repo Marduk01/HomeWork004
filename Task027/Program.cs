7b1e9f203a4fd7be9d667fab398f12cbd74fb99d
Igor Bodalov <igorbodalov@gmail.com>
2023-03-27 12:06:28 +0300

    Task027

---
+++ Task027/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая 
+// 1. Принимает на вход число и 
+// 2. Выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12;
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumNumbers = SumNumbers(number);
+Console.Write($"Сумма от 1 до {number} = {sumNumbers}");
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i; 
+    }
+    return sum;
+}
