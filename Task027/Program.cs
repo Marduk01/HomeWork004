85532a217b02921368e857e4ae13cbb06fc21aa7
Igor Bodalov <igorbodalov@gmail.com>
2023-03-27 14:18:16 +0300

    Task029

--- Task027/Program.cs
+++ Task027/Program.cs
@@ -7,17 +7,30 @@
 // 9012 -> 12;
 
 Console.Write("Введите число : ");
-int number = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 
-int sumNumbers = SumNumbers(number);
-Console.Write($"Сумма от 1 до {number} = {sumNumbers}");
+int len = Length(a);
+Console.Write($"Сумма цифр в числе {a} равно: {Sum(a,len)}");
 
-int SumNumbers(int num)
+int Length(int a)
+{
+    int index = 0;
+    while(a > 0)
+    {
+        a /= 10;
+        index++;
+    }    
+    return index;
+}
+
+int Sum(int a, int len)
 {
     int sum = 0;
-    for (int i = 1; i <= num; i++)
+    for (int i = 1; i <= len; i++)
     {
-        sum += i; 
+        sum += a % 10;
+        a /= 10;
     }
     return sum;
 }
+
---
+++ Task029/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая 
+// 1. Задаёт массив из 8 элементов и 
+// 2. Выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+
+int[] array = new int[8];
+
+Random nmb = new Random();
+Console.Write("[");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = nmb.Next(0,99);
+    Console.Write($"{array[i]} ");
+}
+
+Console.Write("]");
